7a1085cabe994e33fcef61893781e8c542d1d419
peterbenoit <peterbenoit@gmail.com>
2026-06-28 15:15:11 -0400

    Verify sidepanel

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch(error => console.error("Unable to configure side panel behavior:", error));
+if (chrome.sidePanel?.setPanelBehavior) {
+  chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch(error => console.error("Unable to configure side panel behavior:", error));
+}
